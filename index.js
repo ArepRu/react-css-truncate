e4b248aaa37481f4bd9696716e1dd3323cd833d8
Alexey Repin <arepru@gmail.com>
2019-09-18 13:26:03 +0300

    Simple fixes

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
 /**
  * Simple CSS multiline truncation
- * 
+ *
  * Example:
- * 
+ *
  * import linesTruncate from 'react-css-truncate';
- * 
+ *
  * <div style={linesTruncate(2)}>
  * <div style={{ ...linesTruncate(1), width: '50px' }}>
- * 
+ *
  */
 
-const linesTruncate = (linesCount) => {
+const linesTruncate = linesCount => {
   const lines = parseInt(linesCount);
 
   if (isNaN(lines) || lines < 1) {
     return {};
   }
 
-  return lines === 1 ? {
-    whiteSpace: 'nowrap',
-    textOverflow: 'ellipsis',
-    overflow: 'hidden',
-  } : {
-    '-webkit-line-clamp': `${lines} !important`,
-    '-webkit-box-orient': 'vertical',
-    display: '-webkit-box',
-    maxHeight: '100%',
-    overflow: 'hidden',
-  };
-}
+  return lines === 1
+    ? {
+        whiteSpace: "nowrap",
+        textOverflow: "ellipsis",
+        overflow: "hidden"
+      }
+    : {
+        "-webkit-line-clamp": `${lines} !important`,
+        "-webkit-box-orient": "vertical",
+        display: "-webkit-box",
+        maxHeight: "100%",
+        overflow: "hidden"
+      };
+};
 
-module.export = linesTruncate;
+export default linesTruncate;
